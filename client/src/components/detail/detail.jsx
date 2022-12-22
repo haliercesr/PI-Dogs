641c0183dcdf43251232b47300522b3366d3fb8b
cesar <haliercesr@gmail.com>
2022-12-22 09:21:36 -0300

    listo

--- client/src/components/detail/detail.jsx
+++ client/src/components/detail/detail.jsx
@@ -16,39 +16,37 @@ function Detail(props) {
 
    const [dog, setDog] = useState([]);
 
-   const zoomIn=()=>{
+   const zoomIn = () => {
       setZoomImage(true)
    }
 
-   const zoomOut=()=>{
+   const zoomOut = () => {
       setZoomImage(false)
    }
- 
-   useEffect(() => {                       
-         if (allDogs.length>0) { 
-            const data=allDogs.filter(dog=>dog.id===Number(id))                   
-            setDog(data);
-          
-         }
-   }, [id,allDogs]);
+
+   useEffect(() => {
+      if (allDogs.length > 0) {
+         const data = allDogs.filter(dog => dog.id === Number(id))
+         setDog(data);
+
+      }
+   }, [id, allDogs]);
 
 
    return (
       <div className={styles.DetailContainerCard}>
          {allDogs.length === 0 ? <LoadingComponent /> : null}
          {dog[0] && (
+             
             <div className={styles.container}>
-               {zoomImage && <div className={styles.containerImgZoom}> <img onClick={zoomOut} className={styles.imgZoom} src={dog[0].image.url} alt="dog"/></div>}
-               <div className={styles.Detail}>
-                  <div className={styles.titleDetail}>
-                     <h1 className={styles.titleDet}> {dog[0].name}</h1>
-                  </div>
-                  <div className={styles.textDetail}>
-                  <p>El {dog[0].name}  mide entre {dog[0].height.metric[0]} y {dog[0].height.metric[1]} centímetros de altura y pesa de {dog[0].weight.metric[0]} a {dog[0].weight.metric[1]} kilogramos.
-                      Su temperamento es una combinación de {dog[0].temperament}. Este encantador canino tiene una esperanza de vida
-                       de {dog[0].life_span}.</p>
-                  </div>
-               </div>
+               {zoomImage && <div className={styles.containerImgZoom}> <img onClick={zoomOut} className={styles.imgZoom} src={dog[0].image.url} alt="dog" /></div>}
+               <div className={styles.NameDog}>{dog[0].name}</div>
+               <div className={styles.AlturaDog}><span>Altura:<p>{dog[0].height.metric[0]}</p></span></div>
+               <div className={styles.PesoDog}>Peso:{dog[0].weight.metric[0]}</div>
+               <div className={styles.lifeDog}>Altura:{dog[0].life_span}</div>
+               <div className={styles.temperamentsDog}>Temperamentos:{dog[0].temperament}</div>
+
+               
                <div className={styles.img}>
                   <img onClick={zoomIn} src={dog[0].image.url} alt="dog"></img>
                </div>
